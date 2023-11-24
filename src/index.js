36cca1fe32b9872f8791fa6d363f374424be37a9
RAGE1712 <137414196+RAGE1712@users.noreply.github.com>
2023-11-24 19:58:25 +0200

    ww

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-aio-3.2.6.min.js';
+import SlimSelect from 'slim-select'
+
+axios.defaults.headers.common['x-api-key'] = 'live_iDoiV0EHJllOQpbTYpgldcvoG2FwbjPi0qHS9L5SZQ11njYtU009ZPQL3TlvYCrZ';
+// new SlimSelect({
+//   select: '#selectElement'
+// })
+
+// console.log(axios.isCancel('something'));
+
+// const refs = {
+//     selector: document.querySelector('.breed-select'),
+//     loader: document.querySelector('.loader'), 
+//     error: document.querySelector('.error'),
+//     div: document.querySelector('.cat-info')
+// };
+
+// refs.selector.addEventListener('change', onBreedSelect);
+
+// onBreedSelect
+
+// fetchCatByBreed(breedId);
