dcfd0046a96c6928d2ff38d08ab1d9ea4945cc9d
RAGE1712 <137414196+RAGE1712@users.noreply.github.com>
2023-11-24 22:57:26 +0200

    rdy

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-aio-3.2.6.min.js';
 import SlimSelect from 'slim-select'
+import 'slim-select/dist/slimselect.css';
+
 import { fetchCatByBreed, fetchBreeds } from './cat-api';
-
-// axios.defaults.headers.common['x-api-key'] = 'live_iDoiV0EHJllOQpbTYpgldcvoG2FwbjPi0qHS9L5SZQ11njYtU009ZPQL3TlvYCrZ';
-// new SlimSelect({
-//   select: '#selectElement'
-// })
-
-// console.log(axios.isCancel('something'));
 
 const refs = {
   selector: document.querySelector('.breed-select'),
@@ -17,48 +12,120 @@ const refs = {
   error: document.querySelector('.error'),
   div: document.querySelector('.cat-info'),
 };
+// axios.defaults.headers.common['x-api-key'] = 'live_iDoiV0EHJllOQpbTYpgldcvoG2FwbjPi0qHS9L5SZQ11njYtU009ZPQL3TlvYCrZ';
+
+
+// console.log(axios.isCancel('something'));
+
+
+// refs.selector.classList.remove('is-hidden');
+// refs.error.classList.add('is-hidden');
 
 fetchBreeds()
   .then(data => {
     const markup = createSelectorMarkup(data);
+    refs.error.classList.add('is-hidden');
     refs.selector.innerHTML = markup;
+    refs.selector.classList.remove('is-hidden');
+    refs.loader.classList.remove('loader');
     // console.log(markup)
-  })
-    .catch(error => console.log(error));
+    new SlimSelect({
+      select: refs.selector,
 
-function createSelectorMarkup(arr) {
-      return arr
-        .map(
-          ({ id, name }) => `
-     <option value="${id}">${name}</option>`
-        )
-        .join('');
-}
+      settings: {
+        // Below are a list of optional fields
+        // their values are the defaults
+        disabled: false,
+        alwaysOpen: false,
+        showSearch: false,
+        searchPlaceholder: '',
+        searchText: '',
+        searchingText: '',
+        searchHighlight: false,
+        closeOnSelect: true,
+        // contentLocation: document.body,
+        contentPosition: 'absolute',
+        openPosition: 'auto', // options: auto, up, down
+        placeholderText: '',
+        allowDeselect: false,
+        hideSelected: false,
+        showOptionTooltips: false,
+        minSelected: 0,
+        maxSelected: 1000,
+        timeoutDelay: 200,
+        maxValuesShown: 20,
+        maxValuesMessage: '{number} selected',
+      },
+    });
+  })
+    .catch(error => {
+        refs.error.classList.remove('is-hidden');
+            refs.selector.classList.add('is-hidden');
+            refs.loader.classList.remove('loader');
+        Notiflix.Notify.failure(
+          `❌ Oops! Something went wrong! Try reloading the page! ❌`
+        );
+        console.log(error)
+    });
+
     
 //!===========================Descriprion================================= !// 
 
 refs.selector.addEventListener('change', onBreedSelect);
 
 function onBreedSelect(event) {
-  const id = event.target.value;
-  fetchCatByBreed(id)
-    .then(data => {
-      refs.div.innerHTML = createDescriptionMarkup(data);
-      // console.log(createDescriptionMarkup(data))
-    })
-    .catch(err => console.log(err));
+    const id = event.target.value;
+    refs.error.classList.add('is-hidden');
+    refs.div.classList.add('is-hidden');
+    refs.loader.classList.add('loader');
+    fetchCatByBreed(id)
+        .then(data => {
+            refs.div.innerHTML = createDescriptionMarkup(data);
+            refs.div.classList.remove('is-hidden');
+            refs.loader.classList.remove('loader');
+            // console.log(createDescriptionMarkup(data))
+        }
+   
+      )
+      .catch(err => {
+          refs.error.classList.remove('is-hidden');
+           refs.selector.classList.add('is-hidden');
+           refs.loader.classList.remove('loader');
+          Notiflix.Notify.failure(
+            `❌ Oops! Something went wrong! Try reloading the page! ❌`
+          );
+        console.log(err);
+      });
+   
+
 }
 
+
+        
 function createDescriptionMarkup(arr) {
   return arr
     .map(
       ({ url, breeds }) => `
+    <span class="cat-card">
      <img src="${url}" alt="${breeds[0].name}" class="cat-image">
+     <span class="cat-descr">
      <h1 class="cat-name">${breeds[0].name}</h1>
      <h2 class="cat-temperament">${breeds[0].temperament}</h2>
-     <h3 class="cat-description">${breeds[0].description}</h3>`
+     <p class="cat-description">${breeds[0].description}</p>
+     </span></span>`
     )
     .join('');
 }
+function createSelectorMarkup(arr) {
+  return arr
+    .map(
+      ({ id, name }) => `
+     <option value="${id}">${name}</option>`
+    )
+    .join('');
+}
+
+
+//!===========================SlimSelect================================= !// 
 
 
